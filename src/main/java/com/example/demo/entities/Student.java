7b1dc6fa2cc25e125722fdad6c8e1b21c9b04f05
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-29 09:13:52 +0100

    Update Student.java

--- src/main/java/com/example/demo/entities/Student.java
+++ src/main/java/com/example/demo/entities/Student.java
@@ -1,5 +1,25 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+long id;
+
+public long getId() {
+	return id;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+
+
+
 
 }
